8ea661b151538fc5ac0de9318f41b2e31b8ff26b
Jakob <jped22@student.aau.dk>
2026-04-10 10:48:21 +0200

    Noget

--- Assets/Jakob/MagneticTest.cs
+++ Assets/Jakob/MagneticTest.cs
@@ -1,16 +1,29 @@
+using UnityEditor.Searcher;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics.HapticsUtility;
 
 public class MagneticTest : MonoBehaviour
 {
+    [SerializeField] GameObject playerBody;
+    Rigidbody rb;
+    CharacterController controller;
+    public float pullStrength = 1;
+
     LayerMask layerMask;
 
     bool magnetizeable = false;
+
+    public bool isActivated;
+    public Vector2 gripValue;
 
     [SerializeField]
     GameObject lightOnObject, lightOffObject;
 
     void Start()
     {
+        //rb = playerBody.GetComponent<Rigidbody>();
+        controller = playerBody.GetComponent<CharacterController>();
         layerMask = LayerMask.GetMask("Magnetic");
         lightOnObject.SetActive(false);
         lightOffObject.SetActive(true);
@@ -32,6 +45,15 @@ public class MagneticTest : MonoBehaviour
 
             magnetizeable = true;
 
+            if (isActivated) // MAGNETIZEMENT TIME !!!!!!!!!!!!
+            {
+                Vector3 directionToWall = (hit.point - playerBody.transform.position).normalized;
+
+
+                controller.Move(directionToWall * pullStrength * Time.fixedDeltaTime);
+                //rb.AddForce(directionToWall * pullStrength, ForceMode.Acceleration);
+            }
+
             lightOnObject.SetActive(true);
             lightOffObject.SetActive(false);
 
@@ -48,5 +70,23 @@ public class MagneticTest : MonoBehaviour
 
             //Debug.Log("Did not Hit");
         }
+
+    }
+
+    public void OnSelect(/*InputValue input*/ InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isActivated = true;
+        }        
+
+        if (context.canceled)
+        {
+            isActivated = false;
+        }
+
+        //gripValue = input.Get<Vector2>();
+
+        Debug.Log("I work!");
     }
 }
